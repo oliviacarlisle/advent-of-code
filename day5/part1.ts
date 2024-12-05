1e79464bd24cc945dbfd5aa30c1de6eb2d7e9aae
Olivia Carlisle <151247171+oliviacarlisle@users.noreply.github.com>
2024-12-05 12:47:21 -0500

    Refactor day 5 part 1

--- day5/part1.ts
+++ day5/part1.ts
@@ -1,55 +1,25 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { getFileContents } from './utils';
+import { findDivIdx, getFileContents, isSeqValid } from './utils';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const lines: string[] = await getFileContents(path.join(__dirname, 'input.txt'));
-if (lines[lines.length - 1].trim() === '') lines.pop(); // remove last empty line
+
 // find the blank line that divides the sections
-let divIdx: number = 0;
+const divIdx = findDivIdx(lines);
 
-for (let i = 0; i < lines.length; i++) {
-  if (lines[i] === '') {
-    divIdx = i;
-    break;
-  } 
-}
-
-const rules: string[] = lines.slice(0, divIdx);
-const sequences: string[] = lines.slice(divIdx + 1);
+// load the rules and sequences into memory
+const rules: number[][] = lines.slice(0, divIdx).map((line: string) => line.split('|').map((num: string) => Number(num)));
+const sequences: number[][] = lines.slice(divIdx + 1).map((line: string) => line.split(',').map((num: string) => Number(num)));
 
 if (rules.length + sequences.length + 1 === lines.length) console.log('PASS');
 
 let sum = 0;
-
 // check each sequence
-for (let i = 0; i < sequences.length; i++) {
-  const seq: number[] = sequences[i].split(',').map((num: string) => Number(num));
-
-  // index the sequence using a Map
-  const seqIdx = new Map<number, number>(); 
-  seq.forEach((num, i) => seqIdx.set(num, i)) // assume there are no duplicates
-
-  // load the rules into a map
-  const rulesPairs: number[][] = [];
-  for (const rule of rules) {
-    const [a, b]: number[] = rule.split('|').map((num: string) => Number(num));
-    rulesPairs.push([a, b]);
-  }
-
-  let valid = true;
-  for (const [a, b] of rulesPairs) {
-    if (seqIdx.has(a) && seqIdx.has(b)) {
-      if (seqIdx.get(b)! < seqIdx.get(a)!) {
-        valid = false;
-        break;
-      }
-    }
-  }
-
-  if (valid) {
-    sum += (seq[Math.floor(seq.length / 2)])
+for (const seq of sequences) {
+  if (isSeqValid(seq, rules)) {
+    sum += (seq[Math.floor(seq.length / 2)]) // add the middle element
   }
 }
 
--- day5/utils.ts
+++ day5/utils.ts
@@ -3,8 +3,40 @@ import { readFile } from 'node:fs/promises';
 export async function getFileContents(path: string): Promise<string[]> {
   try {
     const content = await readFile(path, 'utf8');
-    return content.split('\n');
+    const raw: string[] = content.split('\n');
+    if (raw[raw.length - 1].trim() === '') raw.pop(); // remove last empty line
+    return raw;
   } catch (err) {
     throw new Error('error reading file', err);
   }
+}
+
+// find the blank line that divides the sections
+export function findDivIdx(lines: string[]) {
+  let divIdx: number = 0;
+  for (let i = 0; i < lines.length; i++) {
+    if (lines[i] === '') {
+      divIdx = i;
+      break;
+    } 
+  }
+  return divIdx;
+}
+
+export function isSeqValid(sequence: number[], rules: number[][]) {
+  // index the sequence using a Map
+  const seqIdx = new Map<number, number>(); 
+  sequence.forEach((num, i) => seqIdx.set(num, i)) // assume there are no duplicates
+
+  let valid = true;
+  for (const [a, b] of rules) {
+    if (seqIdx.has(a) && seqIdx.has(b)) {
+      if (seqIdx.get(b)! < seqIdx.get(a)!) {
+        valid = false;
+        break;
+      }
+    }
+  }
+
+  return valid;
 }
